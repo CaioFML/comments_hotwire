9176e62712a2f4b89daed5e9050ede987ee6b296
CaioFML <caio.felipemotta@hotmail.com>
2021-03-12 00:19:36 -0300

    fix stimulus controller

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
 	static targets = ["button"]
 
 	reset() {
-		console.log("EU TO AQUIIIIIIIIIIIIIIIIIIIIIII")
 		this.element.reset()
 		this.buttonTarget.disabled = false
 	}
